b4570b501bf36426104cb23b013a4ad4b18229a5
YarynaPelekh <yaryna.pelekh@hebronsoft.com>
2022-08-02 20:27:57 +0300

    forgot password, submit button, incorrect input rendering

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,21 +1,82 @@
+import React, { useState, useEffect } from "react";
+
+import Button from "../UI/Button";
 import Card from "../UI/Card";
 import Input from "../UI/Input";
 
+import { emailDomain as emailDomain } from "../config/constants";
+
 import classes from "./Login.module.css";
+import classesButton from "../UI/Button.module.css";
 
 const LogIn = () => {
+  const [emailIsValid, setEmailIsValid] = useState(true);
+  const [passwordIsValid, setPasswordIsValid] = useState(true);
+  const [attemptsFinished, setAttemptsFinished] = useState(false);
+
+  const emailRef = React.createRef();
+  const passwordRef = React.createRef();
+
+  const submitHandler = () => {
+    const enteredEmail = emailRef.current.value.trim();
+    const enteredPassword = passwordRef.current.value.trim();
+
+    setEmailIsValid(enteredEmail.endsWith(emailDomain));
+    setPasswordIsValid(enteredPassword.length > 0);
+  };
+
+  useEffect(() => {
+    console.log("emailIsValid", emailIsValid);
+    console.log("passwordIsValid", passwordIsValid);
+    if (!passwordIsValid || !emailIsValid) {
+      let incorrectAttemptsNumber = +localStorage.getItem("incorrectAttemptsNumber") || 0;
+      console.log(incorrectAttemptsNumber);
+      localStorage.setItem("incorrectAttemptsNumber", ++incorrectAttemptsNumber);
+      {
+        incorrectAttemptsNumber >= 3 && setAttemptsFinished(true);
+      }
+    }
+
+    if (passwordIsValid && emailIsValid) {
+      localStorage.setItem("incorrectAttemptsNumber", 0);
+    }
+  }, [emailIsValid, passwordIsValid]);
+
   return (
     <Card>
       <p className={classes.title}>Login</p>
       <div className={classes.inputsContainer}>
-        <Input placeholder="Company Email" label="Email" />
-        <Input placeholder="Password" label="Email" />
-        {/* <div className={classes.inputGroup}>
-          <input className={classes.input} placeholder="Company Email" />
-          <label>Email</label>
-        </div> */}
-        {/* <input className={classes.input} placeholder="Password" label="Password" /> */}
+        <div className={classes.inputWithError}>
+          <Input
+            ref={emailRef}
+            placeholder="Company Email"
+            label="Email"
+            isValid={emailIsValid}
+            onChange={() => setEmailIsValid(true)}
+          />
+          {!emailIsValid && <p className={classes.errorMessage}>Incorrect email</p>}
+          {/* <p className={classes.errorMessage}>{!emailIsValid && "Incorrect email"}</p> */}
+        </div>
+        <div className={classes.inputWithError}>
+          <Input
+            ref={passwordRef}
+            placeholder="Password"
+            label="Password"
+            isValid={passwordIsValid}
+            onChange={() => setPasswordIsValid(true)}
+          />
+          {!passwordIsValid && <p className={classes.errorMessage}>Incorrect password</p>}
+        </div>
       </div>
+      <div className={classes.buttonContainer}>
+        <Button title="Forgot password?" className={`${classesButton.button} ${classesButton.secondaryButton}`} />
+        <Button
+          title="Submit"
+          className={`${classesButton.button} ${classesButton.mainButton}`}
+          onClick={submitHandler}
+        />
+      </div>
+      {attemptsFinished && <p className={classes.errorMessage}>You made tree wrong attempts</p>}
     </Card>
   );
 };
--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,12 +1,34 @@
+import React, { useState } from "react";
+
 import classes from "./Input.module.css";
 
-const Input = (props) => {
+const Input = React.forwardRef((props, ref) => {
+  const [isPasswordMode, setIsPasswordMode] = useState(false);
+
+  const passwordModeClickHandle = () => {
+    setIsPasswordMode((mode) => !mode);
+  };
+
   return (
     <div className={classes.inputGroup}>
-      <input className={classes.input} placeholder={props.placeholder} />
+      <input
+        ref={ref}
+        className={`${classes.input} ${!props.isValid ? classes.inputInvalid : ""}`}
+        placeholder={props.placeholder}
+        type={isPasswordMode ? "password" : "text"}
+        onChange={props.onChange}
+      />
       <label>{props.label}</label>
+      <button
+        className={
+          !props.isValid
+            ? classes.invalidIcon
+            : `${classes.btnEye} ${isPasswordMode ? classes.eyeIcon : classes.crossedEyeIcon}`
+        }
+        onClick={passwordModeClickHandle}
+      ></button>
     </div>
   );
-};
+});
 
 export default Input;
