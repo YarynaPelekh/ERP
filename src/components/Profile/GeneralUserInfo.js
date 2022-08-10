00f9054ac35f980b964d10f5e50a099829a56168
YarynaPelekh <yaryna.pelekh@hebronsoft.com>
2022-08-10 18:17:02 +0300

    leveled all data inside Profile General Card

--- src/components/Profile/GeneralUserInfo.js
+++ src/components/Profile/GeneralUserInfo.js
@@ -1,3 +1,5 @@
+import InfoItemBlock from "../UI/InfoItemBlock";
+
 import classes from "./GeneralUserInfo.module.css";
 import avatar from "../../asserts/helper/avatar.jpg";
 
@@ -7,22 +9,16 @@ const GeneralUserInfo = () => {
       <div className={classes.avatar}>
         <img src={avatar} alt="Avatar" className={classes.avatar} />
       </div>
-      <div>
-        <p>Name</p>
-        <p>Anna</p>
-        <p>Surname</p>
-        <p>Doe</p> <p>Name</p>
-        <p>Anna</p>
-        <p>Surname</p>
-        <p>Doe</p> <p>Name</p>
-      </div>
-      <div>
-        <p>Anna</p>
-        <p>Surname</p>
-        <p>Doe</p> <p>Name</p>
-        <p>Anna</p>
-        <p>Surname</p>
-        <p>Doe</p>
+      <div className={classes.infoContainer}>
+        <div className={classes.infoColumn}>
+          <InfoItemBlock label="Name" value="Anna Anna Anna Anna Anna Anna Anna Anna Anna Anna Anna " />
+          <InfoItemBlock label="Surname" value="Doe" />
+          <InfoItemBlock label="Date of birth" value="12 december 1991" editable={true} />
+        </div>
+        <div className={classes.infoColumn}>
+          <InfoItemBlock label="Email" value="annaannaannaannaanna.doe@henronsoft.com" />
+          <InfoItemBlock label="Phone number" value="+38-098-765-43-21" editable={true} />
+        </div>
       </div>
     </div>
   );
---
+++ src/components/UI/InfoItemBlock.js
@@ -0,0 +1,60 @@
+import { useState, useRef } from "react";
+
+import classes from "./InfoItemBlock.module.css";
+
+const InfoItemBlock = (props) => {
+  const inputRef = useRef();
+
+  const [isEditing, setIsEditing] = useState(false);
+  const [inputValue, setInputValue] = useState(props.value);
+
+  const onChangeHandler = () => {
+    setInputValue(inputRef.current.value);
+  };
+
+  const onKeyDownHandle = (event) => {
+    if (event.keyCode === 13 || event.keyCode === 27) {
+      document.activeElement.blur();
+    }
+  };
+
+  return (
+    <div>
+      <label htmlFor="input" className={classes.label}>
+        {props.label}
+      </label>
+      <div className={classes.alignInputButton}>
+        <input
+          id={props.value}
+          name={props.value}
+          type="text"
+          ref={inputRef}
+          value={inputValue}
+          className={classes.input}
+          disabled={!props.editable}
+          contentEditable={!props.editable}
+          onChange={onChangeHandler}
+          onKeyDown={onKeyDownHandle}
+        />
+
+        {props.editable && (
+          <button
+            className={classes.infoBtn + " " + classes.editBtn}
+            onClick={() => {
+              alert("EDIT ");
+              console.log(document.getElementsByName(props.value));
+              // document.getElementsByName(props.value);
+              inputRef.current.focus();
+            }}
+          />
+        )}
+      </div>
+    </div>
+  );
+};
+
+InfoItemBlock.defaultProps = {
+  editable: false,
+};
+
+export default InfoItemBlock;
